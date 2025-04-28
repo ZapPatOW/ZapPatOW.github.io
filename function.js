26a367c3b77d670083acd4e06df5b748b2de6a6a
ZapPatOW <vbpackers@gmail.com>
2025-04-28 11:52:15 -0400

    edit structure

--- function.js
+++ function.js
@@ -1,43 +1,40 @@
 class myHeader extends HTMLElement{
-
     connectedCallback(){//for home site
         this.innerHTML = `
         <header>
             <nav class = "header">
                 <ul>
-                    <li><a href="./index.html">Home</a></li>
-                    <li><a href="./pages/ideas.html">Ideas</a></li>
-                    <li><a href="./pages/inProgress.html">In Progress</a></li>
-                    <li><a href="./pages/completed.html">Completed</a></li>
-                    <li><a href="./pages/workspace.html">Workspace</a></li>
+                    <li><a href="./Home.html">Home</a></li>
+                    <li><a href="./Ideas.html">Ideas</a></li>
+                    <li><a href="./InProgress.html">In Progress</a></li>
+                    <li><a href="./Completed.html">Completed</a></li>
+                    <li><a href="./Workspace.html">Workspace</a></li>
                 </ul>
             </nav>
         </header>`;
     }
-    
 }
 customElements.define('my-header',myHeader);
-class myHeader2 extends HTMLElement{//for pages in pages folder
+/*class myHeader2 extends HTMLElement{//for pages in pages folder
 
     connectedCallback(){
         this.innerHTML = `
         <header>
             <nav class = "header">
                 <ul>
-                    <li><a href="../index.html">Home</a></li>
-                    <li><a href="ideas.html">Ideas</a></li>
-                    <li><a href="inProgress.html">In Progress</a></li>
-                    <li><a href="completed.html">Completed</a></li>
-                    <li><a href="workspace.html">Workspace</a></li>
+                    <li><a href="../Home">Home</a></li>
+                    <li><a href="../Ideas">Ideas</a></li>
+                    <li><a href="../InProgress">In Progress</a></li>
+                    <li><a href="../Completed">Completed</a></li>
+                    <li><a href="../Workspace">Workspace</a></li>
                 </ul>
             </nav>
         </header>`;
     }
     
 }
-customElements.define('my-header2',myHeader2);
+customElements.define('my-header2',myHeader2);*/
 class myFooter extends HTMLElement{
-
     connectedCallback(){
         this.innerHTML = `
         <div class = footer>
@@ -45,11 +42,10 @@ class myFooter extends HTMLElement{
             <p>
                 Name: Patrick Meinen<br>
                 Email: patricklmeinen@gmail.com<br>
-                Programming languages: Python, Java, C, HTML, JavaScript, CSS<br>
+                Programming languages: Python, Java, C, HTML, JavaScript, CSS, SQLite<br>
                 Certifications: Back-End Developer - Mimo.org
             </p>
         </div>`;
     }
-    
 }
 customElements.define('my-footer',myFooter);
